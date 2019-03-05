0a6f1814cd6ff11a7c803dde7d25af4d2bab319f
Doomsday46 <Gallifrey46@gmail.com>
2019-03-05 13:51:38 +0300

    Инициализация проекта

---
+++ AspCourse/Models/Tournament.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCourse.Models
+{
+    public class Tournament
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var tournament = obj as Tournament;
+            return tournament != null &&
+                   Id == tournament.Id &&
+                   Name == tournament.Name &&
+                   DateTime == tournament.DateTime;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, DateTime);
+        }
+    }
+}
